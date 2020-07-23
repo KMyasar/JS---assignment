e2de89803575608a3ffd7e0d716ee44ce3f104bd
K M yasar <57797755+KMyasar@users.noreply.github.com>
2020-07-23 16:54:34 +0530

    Update console.js

--- Day_1 Assingment/various_method_of_console/console.js
+++ Day_1 Assingment/various_method_of_console/console.js
@@ -1,12 +1,42 @@
-//log
-console.log("Hi this is javascript");
-//error
+console.log("Hi,This is javascript checkin.....")
+var Object_trail = {name:"yasar",age:"18",hobby:"nothing else"};
+
+//console.log
+console.log(Object_trail);
+
+//console.dir
+console.dir(Object_trail);
+
+//console.error
 console.error("trail error");
-//warn
+
+//console.warn
 console.warn("Warning message");
-//table
+
+//console.tabe
 console.table(["apple","banana","cat","dog"]);
-//time
-console.time("starting time");
-console.log("hi time");
-console.timeEnd("times end");
+
+//console.time
+console.time("Time",1);
+var i;
+for(i =0;i<100000;i++){
+    //none
+}
+console.timeEnd("Time",2);
+
+//console.info
+console.info("Hi!I'm javascript");
+
+//console.trace
+function trace(){
+    console.trace();
+}
+trace()
+function call(){
+    
+    console.count("hi "+user);
+    console.count("hi "+user);
+}
+
+var user = "bob";
+console.log(call(user));
