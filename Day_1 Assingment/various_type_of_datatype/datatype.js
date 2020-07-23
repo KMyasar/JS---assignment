de5e9ac476c586a57f144ad4894715cc64281747
K M yasar <57797755+KMyasar@users.noreply.github.com>
2020-07-23 17:12:32 +0530

    Update datatype.js

--- Day_1 Assingment/various_type_of_datatype/datatype.js
+++ Day_1 Assingment/various_type_of_datatype/datatype.js
@@ -1,11 +1,25 @@
+//number
 var number = 34;
+
+//string
 var string = "tie";
+
+//boolean
 var boolean = true;
+
+//undefined
 var undefined ;
-var nunll = null ;
 
-console.log("Number:"+number,"String:"+string,"Boolean:"+boolean,"Undefined:"+undefined,"Null:"+nunll);
+//null 
+var null_ = null ;
 
+console.log("Number:"+number,"String:"+string,"Boolean:"+boolean,"Undefined:"+undefined,"Null:"+null_);
+
+//array
 var thisis_array = ['one','two','three'];
-var thisis_object = {"number":1,"string":"JavaScript"}
-console.log(thisis_array,thisis_object)
+
+//object
+var thisis_object = {"number":1,"string":"JavaScript"};
+
+console.log(`Array :${thisis_array}`);
+console.log("Object "+thisis_object);
