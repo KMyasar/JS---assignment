f3305865db0833848ebc7e37c8a7aa42d8772e40
K M yasar <57797755+KMyasar@users.noreply.github.com>
2020-07-23 18:00:01 +0530

    Add files via upload

---
+++ Day_2 Assingment/asking_user/ask.js
@@ -0,0 +1,11 @@
+var user = confirm("are you 21+ ?");
+if(user==true){
+    var a = window.confirm("can drink");
+    var user1 = "can drink";
+}
+else{
+    window.alert("Oops you are under 21!");
+    var user1 = "cannot drink";
+}
+
+console.log(user1);
